6143579724920efd198a6ad84ef7f0cf7cae87ef
Werik Santos <werik.msti@gmail.com>
2025-06-19 21:19:53 -0300

    changes

---
+++ components/Video/Video.tsx
@@ -0,0 +1,24 @@
+interface VideoProps {
+  source: string; 
+}
+
+const Video = (videoProps: VideoProps) => {
+  const { source } = videoProps;
+
+  return (
+    <video
+      autoPlay
+      muted
+      loop
+      playsInline
+      controls
+      style={{ width: "100%", height: "100%" }}
+    >
+      <source src={source} type="video/mp4" />
+      <track src={source} kind="subtitles"/>
+      Your browser does not support the video tag.
+    </video>
+  );
+};
+
+export default Video;
